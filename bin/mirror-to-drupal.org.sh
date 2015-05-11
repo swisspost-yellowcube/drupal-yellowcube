622a675a0fb181bd32b63afcf33e3529989df01f
Adrian Philipp <mail@adrian-philipp.com>
2015-05-11 15:59:12 +0200

    POSTYC-63: Add drupal repository to known hosts.

--- bin/mirror-to-drupal.org.sh
+++ bin/mirror-to-drupal.org.sh
@@ -5,6 +5,12 @@
 # The ssh key is decrypted in the .travis.yml file and written to `~/.ssh/id_rsa`
 
 CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+DRUPAL_HOST="git.drupal.org"
+
+# Add drupal.org to known keys
+if [ -z `ssh-keygen -F $DRUPAL_HOST` ]; then
+  ssh-keyscan -H $DRUPAL_HOST >> ~/.ssh/known_hosts
+fi
 
 git remote add drupal adrip@git.drupal.org:project/yellowcube.git
 git push drupal $CURRENT_BRANCH --tags
