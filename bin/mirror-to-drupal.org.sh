255036caf597cc522d48686a94f3ce44b87379c2
Adrian Philipp <mail@adrian-philipp.com>
2015-05-12 08:33:59 +0200

    POSTYC-63: Use travis branch instead of current branch.
    On travis the repository is in a detached state. Checkout the
    current branch and push it to drupal.org.

--- bin/mirror-to-drupal.org.sh
+++ bin/mirror-to-drupal.org.sh
@@ -4,7 +4,8 @@
 #
 # The ssh key is decrypted in the .travis.yml file and written to `~/.ssh/id_rsa`
 
-CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+#CURRENT_BRANCH=`git rev-parse --abbrev-ref HEAD`
+CURRENT_BRANCH=$TRAVIS_BRANCH
 DRUPAL_HOST="git.drupal.org"
 
 # Add drupal.org to known keys
@@ -12,5 +13,7 @@ if [ -z `ssh-keygen -F $DRUPAL_HOST` ]; then
   ssh-keyscan -H $DRUPAL_HOST >> ~/.ssh/known_hosts
 fi
 
+echo "Mirroring $CURRENT_BRANCH to $DRUPAL_HOST..."
 git remote add drupal adrip@git.drupal.org:project/yellowcube.git
+git checkout -f $CURRENT_BRANCH
 git push drupal $CURRENT_BRANCH --tags
